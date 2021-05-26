5f4cb4c04adbaa8d3d43c7dcb80794b65e28ce7b
ivan-shargin <Shargin.IA@phystech.edu>
2021-05-26 22:57:23 +0300

    Update unittest_str_into_floatArr.py

--- unittest_str_into_floatArr.py
+++ unittest_str_into_floatArr.py
@@ -1,13 +1,12 @@
- import unittest
- from str_into_floatArr import str_into_floatArr
+import unittest
+from str_into_floatArr import str_into_floatArr
 
- class TestStrIntoFloatArr(unittest.TestCase):
-    def setUp(self):
-        self.str_into_floatArr = str_into_floatArr(s, size)
-
+class TestStrIntoFloatArr(unittest.TestCase):
+    """ Tests function str_into_floatArr """
     def test1(self):
-        self.assertEqual(self.str_into_floatArr('100    12 0.301 -4.12 471 ', 4),
-        [100; 12; 0.301; -4.12])
+        s = "100    12 0.301 -4.12\n"
+        self.assertEqual(str_into_floatArr(s, 4),
+                        [100, 12, 0.301, -4.12])
 
 if __name__ == "__main__":
     unittest.main()
